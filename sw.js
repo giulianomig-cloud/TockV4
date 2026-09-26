b0d3d9c431f08ae52028f7bd62cf82a4980760f1
giulianomig-cloud <giuliano.mig@gmail.com>
2026-09-26 17:06:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 //
 // Cambia CACHE_VERSION ad ogni nuova build pubblicata (tienilo allineato ad
 // APP_VERSION in index.html) così le cache vecchie vengono scartate subito.
-const CACHE_VERSION = "v4-50";
+const CACHE_VERSION = "v4-52";
 const CACHE_NAME = "tock-cache-" + CACHE_VERSION;
 
 const APP_SHELL = [
